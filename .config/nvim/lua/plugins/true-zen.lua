c4311a5cfa41d8ed01214204c64fafdc8943729e
RootOperator <20143055+RootOperator@users.noreply.github.com>
2025-06-02 17:13:54 +0200

    more stuff

--- .config/nvim/lua/plugins/true-zen.lua
+++ .config/nvim/lua/plugins/true-zen.lua
@@ -14,7 +14,7 @@ return {
                         top = 0,
                         bottom = 0,
                     },
-                    callbacks = { -- run functions when opening/closing Ataraxis mode
+                    callbacks = {
                         open_pre = function () vim.cmd("UfoDisable") end,
                         open_pos = nil,
                         close_pre = nil,
@@ -22,11 +22,19 @@ return {
                     },
                 },
                 minimalist = {
-                    callbacks = { -- run functions when opening/closing Ataraxis mode
+                    callbacks = {
                         open_pre = function () vim.cmd("Barbecue hide") end,
                         open_pos = nil,
                         close_pre = nil,
                         close_pos = function() vim.cmd("Barbecue show") end
+                    },
+                },
+                focus = {
+                    callbacks = {
+                        open_pre = function () vim.o.showtabline = 0 end,
+                        open_pos = nil,
+                        close_pre = nil,
+                        close_pos = function() vim.o.showtabline = 1 end
                     },
                 },
             },
