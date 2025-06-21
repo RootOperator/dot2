9dfcc982cef42fd9ca64e1365d9c6898b4314522
RootOperator <20143055+RootOperator@users.noreply.github.com>
2025-06-21 08:50:36 +0200

    more stuff

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -187,7 +187,7 @@ return {
 
             ins_right {
                 'diff',
-                symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+                symbols = { added = ' ', modified = ' ', removed = ' ' },
                 diff_color = {
                     added = { fg = colors.green },
                     modified = { fg = colors.orange },
