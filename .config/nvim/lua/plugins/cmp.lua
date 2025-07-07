65389a85e1720112aa9252287a27ea6ffbb4d9dd
RootOperator <20143055+RootOperator@users.noreply.github.com>
2025-07-07 21:58:51 +0200

    more stuff

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -5,6 +5,7 @@ return {
         'hrsh7th/cmp-nvim-lua',
         'hrsh7th/cmp-nvim-lsp-signature-help',
         'hrsh7th/cmp-path',
+        'hrsh7th/cmp-buffer',
         { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
         'saadparwaiz1/cmp_luasnip'
     },
@@ -42,6 +43,7 @@ return {
                 { name = 'nvim_lsp'},
                 { name = 'nvim_lsp_signature_help'},
                 { name = 'nvim_lua'},
+                { name = 'buffer'},
                 { name = 'luasnip' },
                 { name = 'neorg'},
             },
