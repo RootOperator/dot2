686790c0fb49a297e6b5c7d0bc4d512edb9d38b1
RootOperator <20143055+RootOperator@users.noreply.github.com>
2025-08-26 23:36:09 +0200

    more stuff

--- .config/nvim/lua/plugins/snipe.lua
+++ .config/nvim/lua/plugins/snipe.lua
@@ -1,7 +1,20 @@
 return {
-  "leath-dub/snipe.nvim",
-  keys = {
-    {"gb", function () require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu"}
-  },
-  opts = {}
+    "leath-dub/snipe.nvim",
+    keys = {
+        {"gb", function () require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu"}
+    },
+    opts = {
+        navigate = {
+            ui = {
+                position = "center"
+            },
+            leader = " ",
+            leader_map = {
+                ["s"] = function (m, i) require("snipe").open_vsplit(m, i) end,
+                ["i"] = function (m, i) require("snipe").open_split(m, i) end,
+            },
+            open_vsplit = "S",
+            open_split = "I",
+        }
+    }
 }
